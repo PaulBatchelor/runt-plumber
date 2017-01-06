9b83a8ed9ce399fe8799e1be0bccc23e8590e538
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-01-06 13:59:06 -0800

    missing headers

---
+++ stream.h
@@ -0,0 +1,49 @@
+typedef struct plumber_word {
+    uint32_t type;
+    SPFLOAT flt;
+    char *str;
+    struct plumber_word *next;
+} plumber_word;
+
+typedef struct {
+    plumber_word root;
+    plumber_word *last;
+    uint32_t size;
+} plumber_stream;
+
+/* word: a to-be plumber_pipe */
+int plumber_word_alloc(plumber_data *pd, plumber_word **word);
+int plumber_word_init(plumber_data *pd, plumber_word *word);
+int plumber_word_free(plumber_data *pd, plumber_word *word);
+
+/* stream: a to-be plumber_pipes */
+int plumber_stream_init(plumber_data *pd, plumber_stream *stream);
+int plumber_stream_append(plumber_data *pd, 
+        plumber_stream *stream, plumber_word *word);
+int plumber_stream_destroy(plumber_data *pd, plumber_stream *stream);
+
+/* data types to append to the stream */
+int plumber_stream_append_string(plumber_data *pd, 
+        plumber_stream *stream, const char *str, uint32_t size);
+int plumber_stream_append_ugen(plumber_data *pd, 
+        plumber_stream *stream, const char *key);
+int plumber_stream_append_float(plumber_data *pd, 
+        plumber_stream *stream, SPFLOAT flt);
+
+/* get the function id of a ugen */
+int plumber_str_to_ugen(plumber_data *pd, const char *str, uint32_t *id);
+
+/* parse a string and add it to the stream */
+int plumber_stream_parse_string(plumber_data *pd, 
+        plumber_stream *stream, const char *str);
+
+/* parses a stream and converts it to plumbing */
+int plumbing_parse_stream(plumber_data *pd, 
+        plumbing *pipes, 
+        plumber_stream *stream);
+
+/* this is the function that replaces plumber_reparse_string */
+int plumber_reparse_stream(plumber_data *pd, plumber_stream *stream);
+
+/* call this when you want to recompile */
+int plumber_recompile_stream(plumber_data *pd, plumber_stream *str);
